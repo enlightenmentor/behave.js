1a486f4ec604f653c2c8a9a0b3cad475ca18502d
Tom Fales <fales.tom.1994@gmail.com>
2018-02-28 21:33:18 +0200

    focusable

--- src/focusable.js
+++ src/focusable.js
@@ -1,5 +1,19 @@
-function focusable() {
-  console.log('I will be focusable!');
+import observable from './observable.js';
+
+/**
+ * Adds focusable behavior to HTML element.
+ * 
+ * @param {HTMLElement} node
+ * @param {number} [tabindex=-1] Value of tabindex attribute. Defaults to -1
+ * @returns {HTMLElement} Same HTML element but with additional functionality
+ */
+function focusable(node, tabindex=-1) {
+  node.setAttribute('data-focusable', '');
+  node.setAttribute('tabindex', tabindex);
+  const obs = observable(node);
+  obs.on('focus', () => node.setAttribute('data-focused', ''));
+  obs.on('blur', () => node.removeAttribute('data-focused'));
+  return node;
 }
 
 export default focusable;
